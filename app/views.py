4f0fae484b24cf8339ced77fee0446e9f9a72e4b
Mikhail Bazhenov <rnby.mike@gmail.com>
2014-03-21 01:04:31 +0400

    Add basic templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Mike'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in St Petersburg'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avatar movie was so cool!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avatar movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
