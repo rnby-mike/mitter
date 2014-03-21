a07e1afce4eeb03e01d48a82604547bfccca92a0
Mikhail Bazhenov <rnby.mike@gmail.com>
2014-03-22 01:56:22 +0400

    Add login form

---
+++ config.py
@@ -0,0 +1,9 @@
+CSRF_ENABLED = True
+SECRET_KEY = '3?KS\x04\xf5\xce\xa5\xc1\xb3M\xec\xb7\xa7\xb0\xe0\xf2Yl\xa0vz\xc2\x99'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
