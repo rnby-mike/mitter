c79a977aa5d0a72134a8c9254c9aec1ebec81a41
Mikhail Bazhenov <rnby.mike@gmail.com>
2014-03-23 01:04:14 +0400

    Add models and db helpers scripts

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 CSRF_ENABLED = True
 SECRET_KEY = '3?KS\x04\xf5\xce\xa5\xc1\xb3M\xec\xb7\xa7\xb0\xe0\xf2Yl\xa0vz\xc2\x99'
 
